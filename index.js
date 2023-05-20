6dd7d5c3dc3ac195ed0921c6f7276c443379a92d
shuvo9620 <smahfuzur0@gmail.com>
2023-05-20 15:27:22 +0600

    toy details functionality by id done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,6 +36,13 @@ async function run() {
         app.get('/toys', async (req, res) => {
             const cursor = toyCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/toydetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toyCollection.findOne(query);
             res.send(result);
         })
 
